3ee78eb5a27f6cab484f34b3505f87363753430c
Lafoerte91 <lafoerte@gmail.com>
2024-02-22 22:32:26 +0300

    Carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Плавные якори к ссылкам
 const links = document.querySelectorAll('a[href^="#"]');
 
 links.forEach(link => {
@@ -12,4 +13,57 @@ links.forEach(link => {
       })
     }
   })
+})
+
+// Карусель
+const cards = document.querySelectorAll('.stages-mobile__card')
+const circles = document.querySelectorAll('.stages-mobile__circle')
+let currentCardIndex = 0
+
+function showCard(index) {
+  cards.forEach((card, i) => {
+    card.classList.remove('active')
+    if(i === index) {
+      card.classList.add('active')
+    }
+    if(index == 0) {
+      document.getElementById('prevBtn').style.backgroundColor = '#D9D9D9'
+    } else {
+      document.getElementById('prevBtn').style.backgroundColor = 'rgb(49, 49, 49)'
+    }
+    if(index == cards.length - 1) {
+      document.getElementById('nextBtn').style.backgroundColor = '#D9D9D9'
+    } else {
+      document.getElementById('nextBtn').style.backgroundColor = 'rgb(49, 49, 49)'
+    }
+  }) 
+  circles.forEach((circle, i) => {
+    circle.classList.remove('active')
+    if(i === index) {
+      circle.classList.add('active')
+    }
+  })
+}
+
+document.getElementById('prevBtn').addEventListener('click', function() {
+  currentCardIndex = Math.max(currentCardIndex - 1, 0)
+  showCard(currentCardIndex)
+})
+
+document.getElementById('nextBtn').addEventListener('click', function() {
+  currentCardIndex = Math.min(currentCardIndex + 1, cards.length - 1)
+  showCard(currentCardIndex)
+})
+
+// Показываем первую карточку из карусели при загрузке страницы
+function showFirstCard() {
+  const cards = document.querySelectorAll('.stages-mobile__card')
+  const circles = document.querySelectorAll('.stages-mobile__circle')
+  cards[0].classList.add('active')
+  circles[0].classList.add('active')
+  document.getElementById('prevBtn').style.backgroundColor = '#D9D9D9'
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  showFirstCard()
 })
